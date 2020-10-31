767e5e14ec500a8157cc35f0957334986e48cffa
DHUAVH <57971469+DHUAVH@users.noreply.github.com>
2020-10-31 14:30:12 +0800

    ‘variable parameter'

--- args&kw.py
+++ args&kw.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*-
-def product1(*args):             # 可变参数
+def func1(*args):             # 可变参数
     r = 1
     for x in args:
         r = r * x
     print(r)
-    return
 
-product1(1,2,3)
+func1(1,2,3)         #直接传参
+func1(*[1,2,3])      #先组装成list
+func1(*(1,2,3))      #先组装成tuple
 
-def product2(**kw):             # 关键字参数
-    for key,value in kw:
-        print('key='+key,'value='+value)
-    return
+def func2(**kw):             # 关键字参数
+    for key, value in kw.items():
+        print('%s %d' % (key,value))
 
-product2(a='1',b='2',c='3')
+func2(a=1,b=2)                #直接传参
+func2(**{'a': 1, 'b': 2})     #先组装成dict  
